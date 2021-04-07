26da7a05f1edbd671081d25a4b2ecdd1544501b7
Roman Prokhorov <daiverr1@gmail.com>
2021-04-07 17:24:06 +0300

    change UI details (for example: replace emoji by UIImage)

--- CS193/ViewController.swift
+++ CS193/ViewController.swift
@@ -23,8 +23,9 @@ class ViewController: UIViewController {
     @IBOutlet var cardButtons: [UIButton]!
     
     var emojiChoices = ["🦁","🐷","🐱","🐶", "🐭", "🐵", "🐸", "🐰"]
+    var pictureChoices: [UIImage] = [UIImage(named: "Roman")!, UIImage(named: "Zaval")!,UIImage(named: "Nata")!,UIImage(named: "Ilia")!,UIImage(named: "Bar")!,UIImage(named: "Artur")!,UIImage(named: "Andrey")!]
     
-    var emoji = [Int:String]()
+    var emoji = [Int:UIImage]()
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
@@ -42,21 +43,25 @@ class ViewController: UIViewController {
             let button = cardButtons[index]
             let card = game.cards[index]
             if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                //button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.setImage(emoji(for: card), for: UIControl.State.normal)
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                //button.setImage(nil, for: UIControl.State.normal)
             } else {
                 button.setTitle("", for: UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9372549057, green: 0.4626568195, blue: 0.2875248011, alpha: 0) : #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+                //button.setImage(UIImage(named: "Image"), for: UIControl.State.normal)
+                card.isMatched ? button.setImage(nil, for: UIControl.State.normal) : button.setImage(UIImage(named: "Image"), for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9372549057, green: 0.4626568195, blue: 0.2875248011, alpha: 0) : #colorLiteral(red: 0.9996142983, green: 0.9494145513, blue: 0.01051531732, alpha: 1)
             }
         }
     }
     
-    func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    func emoji(for card: Card) -> UIImage {
+        if emoji[card.identifier] == nil, pictureChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(pictureChoices.count)))
+            emoji[card.identifier] = pictureChoices.remove(at: randomIndex)
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card.identifier] ?? UIImage(named: "Roman")!// as! UIImage
     }
     
 }
